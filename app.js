1012f56aacd9475d27ced24091849fa5f35da2fd
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-04-24 18:24:32 -0400

    random color generator function

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const container = document.querySelector('#container');
 
 //buttons
-const resetButton = document.querySelector('.reset');
+const clearButton = document.querySelector('.clear');
 const blackButton = document.querySelector('.black');
 const rgbButton = document.querySelector('.rgb');
 
-//create a grid
+//create 16x16 grid
 
 for(let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
-    container.appendChild(div);
     div.style.cssText = 'border: 0.25px solid black; height: 25px; width: 25px';
+    div.classList.add('box');
+    div.textContent = '';
+    container.appendChild(div);
 
-    div.addEventListener('click', e => {
-        e.target.style.cssText = 'background-color: black';
+    const boxes = document.querySelectorAll('.box');
+
+    //random color generator for RGB button
+    const makeRandomColor = () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        div.addEventListener('click', e => {
+            div.style.backgroundColor = "#" + randomColor;
+        });
+    };
+
+    //add eventListener for color change
+    rgbButton.addEventListener('click', makeRandomColor);
+
+    blackButton.addEventListener('click', () => {
+        div.addEventListener('click', e => {
+            div.style.backgroundColor = 'black';
+        });
     });
-}
-
+};
